2c6bfa8c1bf6bab48dea0367b81e5c93adc16cce
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2016-08-10 00:10:02 +0900

    audio context

--- index.js
+++ index.js
@@ -2,38 +2,70 @@ var onFailSoHard = function(e){
   console.log('rejected', e);
 };
 
-var getUserMedia = function(opt, onSuccess, onFail){
-  if(navigator.getUserMedia){
-    navigator.getUserMedia(opt, onSuccess, onFail);
-  } else if (navigator.mozGetUserMedia) {
-    navigator.mozGetUserMedia(opt, onSuccess, onFail);
-  } else if (navigator.webkitGetUserMedia) {
-    navigator.webkitGetUserMedia(opt, onSuccess, onFail);
-  } else if (navigator.msGetUserMedia) {
-    navigator.msGetUserMedia(opt, onSuccess, onFail);
-  } else {
-    console.error('getUserMedia not supported in your browser');
-  }
-}
 
-getUserMedia(
-  {video: true},
-  function(localMediaStream){
-    var video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(localMediaStream);
-  },
-  onFailSoHard);
+navigator.getUserMedia = navigator.getUserMedia       ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var video = document.querySelector('video');
 var srcCanvas = document.querySelector('.src-canvas');
 var srcContext = srcCanvas.getContext('2d');
 var destCanvas = document.querySelector('.dest-canvas');
 var destContext = destCanvas.getContext('2d');
-
 var worker = new Worker('./worker.js');
 worker.addEventListener('message', function(e){
   destContext.putImageData(e.data, 0, 0);
 });
+
+var freqCanvas = document.querySelector('.freq-canvas');
+var freqContext = freqCanvas.getContext('2d');
+
+var width = freqCanvas.width;
+var height = freqCanvas.height;
+
+var context = new AudioContext();
+var analyzer = context.createAnalyser();
+analyzer.fftSize = 256;
+
+function drawFreq(){
+  var data = new Float32Array(analyzer.frequencyBinCount / 4);
+  analyzer.getFloatFrequencyData(data);
+
+  analyzer.maxDecibels = 0;
+  analyzer.minDecibels = -60;
+
+  freqContext.fillStyle = 'rgb(0, 0, 0)';
+  freqContext.clearRect(0, 0, width, height);
+
+  var range = analyzer.maxDecibels - analyzer.minDecibels;
+
+  for(var i = 0, len = data.length; i < len; i++){
+    var x = Math.floor(i / len * width);
+    var power = -1 * ((data[i] - analyzer.maxDecibels) / range);
+    var y = Math.floor(power * height);
+    var color = Math.floor(power * 256);
+    var style = 'rgb(' + color + ',' + color + ',' + color + ')';
+    freqContext.fillStyle = style;
+    freqContext.fillRect(x, y, width / len, height - y);
+  }
+  requestAnimationFrame(drawFreq);
+}
+
+navigator.getUserMedia(
+  {video: true, audio: true},
+  function(localMediaStream){
+    // Create the instance of MediaStreamAudioSourceNode
+    var source = context.createMediaStreamSource(localMediaStream);
+    source.connect(analyzer);
+    analyzer.connect(context.destination);
+    drawFreq();
+
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(localMediaStream);
+  },
+  onFailSoHard);
 
 var drawVideo = function(){
   var h = srcCanvas.height;
@@ -49,4 +81,3 @@ function animLoop(){
   requestAnimationFrame(animLoop);
 };
 window.onload = animLoop;
-
--- worker.js
+++ worker.js
@@ -10,15 +10,22 @@ Worker.process = function(imageData){
   return imageData;
 }
 
+function discretize(num, bin){
+  var binSize = 255 / bin;
+  return Math.floor(num / binSize) * binSize;
+}
 function colorToThreshold(color, th){
   for (var i = 0; i < color.length; i += 4) {
     g = 0.299*color[i+0]
       + 0.587*color[i+1]
       + 0.114*color[i+2];
-    if( g > th){
-      color[i+0] = color[i+1] = color[i+2] = 255;
-    } else{
-      color[i+0] = color[i+1] = color[i+2] = 0;
-    }
+    color[i+0] = color[i+1] = color[i+2] = discretize(g, 8) ;
+    /*
+     if( g > th){
+     color[i+0] = color[i+1] = color[i+2] = 255;
+     } else{
+     color[i+0] = color[i+1] = color[i+2] = g;
+     }
+     */
   }
 }
